447eef6c8c772bb696f3dc7f6833b54e1f4f14db
Daniel Cabrera <drocks2000@gmail.com>
2017-05-28 19:26:24 +0200

    Setup time (in order to prevent interrup's locking)

---
+++ watering.ino
@@ -0,0 +1,38 @@
+
+
+
+void setup()
+{
+  Serial.begin(57600);
+  awaitNthSeconds(10);
+}
+
+
+void loop()
+{
+  Serial.println("Tick");
+  delay(1000);
+}
+
+
+void awaitNthSeconds(unsigned int seconds)
+{
+  unsigned long reachTime;
+
+  char message[30];
+  
+  reachTime = millis() + 1000;
+  seconds = 10;
+
+  do {
+    if (millis() > reachTime) {
+      seconds--;
+      reachTime += 1000;
+      sprintf(message, "Esperando %d segundos", seconds);
+      Serial.println(message);
+    }
+    delay(100);
+  } while (seconds);
+
+  Serial.println("Inicializacion completada");
+}
