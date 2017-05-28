efddda5c808e885ca6acb462c1e0bafced8a418b
Daniel Cabrera <drocks2000@gmail.com>
2017-05-28 20:48:31 +0200

    Prepare main loop to read humidity' sensors

--- watering.ino
+++ watering.ino
@@ -2,18 +2,35 @@
 #include <EEPROM.h>
 #include "SensorController.h"
 
+#ifndef int8_t
+  #define int8_t unsigned char;
+#endif
+
+#define SENSORS_CONFIG_ADDR 0x0001
+
+SensorController sensorController;
+
 void setup()
 {
   Serial.begin(57600);
+  sensorController.loadConfig(SENSORS_CONFIG_ADDR);
   awaitNthSeconds(10);
 }
 
 
 void loop()
 {
+  byte sensorId;
+  
+  for (byte sensorId=1; sensorId<=MAX_HUMIDITY_SENSORS; sensorId++) {
+    if (sensorController.isSensorEnabled(sensorId)) {
+      // Enable & read sensor 'sensorId'
+    }
+  }
   Serial.println("Tick");
   delay(1000);
 }
+
 
 
 void awaitNthSeconds(unsigned int seconds)
